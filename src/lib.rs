4a1636460db9c0b0b493a45d3f3a340aac8f8aef
Gabeperson <95374687+Gabeperson@users.noreply.github.com>
2025-05-07 01:43:53 -0700

    lexing mostly done. Just need to do nested lexing for template literals

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+use chumsky::Parser;
+
 // mod ast;
 mod lexer;
 // mod parser;
+
+pub fn lex(s: &str) {
+    dbg!(lexer::lexer().parse(s));
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use hzel::lex;
+
 fn main() {
-    println!("Hello, world!");
+    let f = std::fs::read_to_string("code.txt").unwrap();
+    lex(&f);
 }
